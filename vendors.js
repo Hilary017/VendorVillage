aed96317f3abdfa4a92e310d2ccd7c1e461c52ae
Ugwu Hilary <ugwuhilary017@gmail.com>
2023-09-02 08:09:36 +0100

    vendor profile page added

--- vendors.js
+++ vendors.js
@@ -11,6 +11,9 @@ const thirdToggle = toggleButton.children[2];
 const vendors = document.getElementById("vendors-container");
 const searchBar = document.querySelector(".search_field");
 const searchBtn = document.querySelector(".search-btn");
+const vendorProfile = registerModal.nextElementSibling;
+// 
+// console.log(cancelVendor);
 
 const photography = document.querySelector(".photography"); 
 const allVendors = photography.previousElementSibling;
@@ -76,6 +79,65 @@ const vendorList = [
     },
 ] 
 
+const vendorProfileHandler = (id) => {
+    const vendor = vendorList.filter(vendor => vendor.id === id);
+    const newProfile = document.createElement("div");
+    vendorProfile.innerHTML = "";
+    backdrop.classList.toggle("visible")
+    vendorProfile.classList.toggle("visible");
+   
+
+    for(const profile of vendor) {
+        newProfile.innerHTML = `<div class="vendor-profile" style="width: 90%; padding: 2rem 1rem; padding-bottom: 6rem; position: relative; background-color: #7A3085; margin: 7rem auto 0 auto; border-radius: 10px;">
+                <img src="/images/cake.jpg" alt="TG Bakery" class="vendor--img" style="width: 150px; height: 150px; margin: auto; position: absolute; top: -35%; left: 0%;">
+                <h3 style="text-align: center;">${profile.businessName}</h3>
+                <p style="color: white; text-align: center;">
+                    Mix n Bake Cake is a Lagos number 1 cake company, 
+                    we have changed the wedding cake business with our 
+                    range of aesthetically pleasing and delicious  cake 
+                    that will delight your invitees on your big day.
+                </p>
+                <div style="position: absolute; left: 15px; bottom: 7%;">
+                    <div>
+                        <h4 style="margin-bottom: 0;">Find us on socials</h4>
+                        <div style="margin-top: 0; display: flex; justify-content: center;">
+                            <img src="images/faceb.png" alt="facebook icon" style="width: 30px; height: 30px;">
+                            <img src="images/instagram.png" alt="instagram icon" style="width: 30px; height: 30px;">
+                            <img src="images/whatsapp.png" alt="WhatsApp icon" style="width: 30px; height: 30px;">
+                        </div>
+                    </div>
+                </div>
+                <button id="cancel-vendor" style="position: absolute; right: 20px; bottom: 10%; border: none; padding: 10px; color: white; border-radius: 5px; background-color: #0A86F8;">Book now</button>
+                <button id="book-vendor" style="position: absolute; right: 110px; bottom: 10%; border: none; padding: 10px; color: white; border-radius: 5px; background-color: rgb(94, 94, 95);">Cancel</button>
+            </div>
+            <div style="width: 90%; margin: 0 auto;">
+                <h4>Our Services</h4>
+                <p>Depending on your budget, we can offer you cakes of diferent sizes and 
+                    shape with no compromise. from massive custom wedding cakes specifically 
+                    tailored to your taste, to theme cakes, traditional cakes, cupcake etc.
+                </p>
+                <div>   
+                    <button id="register" style="border: none; padding: 10px; color: white; border-radius: 5px; background-color: #0A86F8;">Photos</button>
+                    <button id="register" style="border: none; padding: 10px; color: white; border-radius: 5px; background-color: black;">Reviews</button>
+                </div>  
+            </div>
+            <div class="service-images" style="width: 90%; margin: 1rem auto;">
+                <img src="/images/cake.jpg" alt="TG Bakery" style="width: 180px; height: 150px;">
+                <img src="/images/cake.jpg" alt="TG Bakery" style="width: 180px; height: 150px;">
+                <img src="/images/cake.jpg" alt="TG Bakery" style="width: 180px; height: 150px;">
+            </div>
+            <div style="width: 90%; margin: 0 auto;">
+                <h3>Contact us</h3>
+                <p>N0 73 Ogunusi Rosd, Ojodu Berger, Lagos</p>
+                <p>Telephone: +2347032122211</p>
+            </div>
+        `
+        vendorProfile.append(newProfile);
+        const cancelVendor = vendorProfile.querySelector("button").nextElementSibling;
+        cancelVendor.addEventListener("click", closeRegistrationModal);
+    }
+}
+
 const loadVendorList = (filterValue = "") => {
     vendors.innerHTML = "";
 
@@ -103,8 +165,8 @@ const loadVendorList = (filterValue = "") => {
             </div>
         `
         vendors.append(newVendor);
-
-        
+        const button = newVendor.querySelector("button");
+        button.addEventListener("click", vendorProfileHandler.bind(null, button.value))
     }
 
     // detailsBtn = vendors.querySelectorAll("button");
@@ -188,8 +250,9 @@ const showDesktopRegistrationModal = () => {
 
 const closeRegistrationModal = (e) => {
     e.preventDefault();
-    backdrop.classList.toggle("visible");
-    registerModal.classList.toggle("visible");
+    backdrop.classList.remove("visible");
+    registerModal.classList.remove("visible");
+    vendorProfile.classList.remove("visible");
     document.body.style.overflow = "auto";
 }
 
